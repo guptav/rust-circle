5d8a21e106882e2e4a156bc9540724cdf2d7fcd9
Vaibhav Gupta <vaibhav.gupta@gmail.com>
2019-04-30 15:11:04 -0700

    guessing game refactoring...

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,34 @@
 /// Guesssing Game
 
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 
 fn main() {
-    println!("Guess the Number");
-    println!("Enter the Number between 1 to 100 ?");
+    println!("Guess the Number (1 to 100)");
+    let secret = rand::thread_rng().gen_range(1, 101);
+    println!("Secret num is {}", secret);
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read the line");
-    println!("You guessed: {}", guess);
+    loop {
+        println!("Enter your guess ?");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read the line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+        }
+    }
 }
 
